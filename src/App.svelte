08a78be559ddb2ab3d39ab839d28ec5fd8e520a4
1seok2 <misohaha31@naver.com>
2021-02-13 23:19:11 +0900

    move files

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import KakaoLogin from "./kakao/KakaoLogin.svelte";
+  import KakaoLogin from "./kakao/login/KakaoLogin.svelte";
 </script>
 
 <KakaoLogin key="9d3c8dc49828939927adf72c361b3165" />
